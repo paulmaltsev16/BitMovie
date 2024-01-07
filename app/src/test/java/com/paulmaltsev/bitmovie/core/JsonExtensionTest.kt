3ac8d9cb194076cacb7e8c937f782c572a8c0d2a
Paul Maltsev <135986470+PaulMaltsev23@users.noreply.github.com>
2024-01-07 20:49:59 +0200

    Create json extension unit test

---
+++ app/src/test/java/com/paulmaltsev/bitmovie/core/JsonExtensionTest.kt
@@ -0,0 +1,40 @@
+import com.google.gson.Gson
+import com.paulmaltsev.bitmovie.core.extensions.fromJson
+import com.paulmaltsev.bitmovie.core.extensions.toJson
+import junit.framework.TestCase
+import org.junit.Test
+
+data class User(
+    val name: String
+)
+
+class JsonExtensionTest {
+    @Test
+    fun `toJson() method should converts list of strings to json`() {
+        // Arrange
+        val inputList = listOf("one", "two", "free")
+
+        // Act
+        val json = inputList.toJson()
+
+        // Assert
+        val expectedJson = Gson().toJson(inputList)
+        TestCase.assertEquals(expectedJson, json)
+    }
+
+    @Test
+    fun `fromJson() method should return array list of object`() {
+        // Arrange
+        val mockJsonString = "[{\"name\":\"Alice\"}, {\"name\":\"Bob\"}]"
+        val expectedResult = arrayListOf(
+            User("Alice"),
+            User("Bob")
+        )
+
+        // Act
+        val result = fromJson<User>(mockJsonString)
+
+        // Assert
+        TestCase.assertEquals(expectedResult, result)
+    }
+}
