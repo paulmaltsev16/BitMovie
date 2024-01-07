c013c90e303bbbe27a7826a37ba8395da0c80498
Paul Maltsev <135986470+PaulMaltsev23@users.noreply.github.com>
2024-01-07 12:13:15 +0200

    Create TmdbScreen

--- app/src/main/java/com/paulmaltsev/bitmovie/features/legalContent/tmdb/TmdbScreen.kt
+++ app/src/main/java/com/paulmaltsev/bitmovie/features/legalContent/tmdb/TmdbScreen.kt
@@ -1,16 +1,55 @@
 package com.paulmaltsev.bitmovie.features.legalContent.tmdb
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import com.paulmaltsev.bitmovie.R
+import com.paulmaltsev.bitmovie.core.extensions.appPadding
+import com.paulmaltsev.bitmovie.core.ui.views.AppSpacer
+import com.paulmaltsev.bitmovie.features.legalContent.tmdb.components.TmdbTopBar
 
 @Composable
-fun TmdbScreen() {
-    Text(
-        "TmdbScreen",
-        modifier = Modifier.fillMaxSize(),
-        textAlign = TextAlign.Center
-    )
+fun TmdbScreen(navController: NavController) {
+    Scaffold(
+        topBar = TmdbTopBar {
+            navController.popBackStack()
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .appPadding()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Image(
+                painterResource(id = R.drawable.logo_tmdb),
+                stringResource(R.string.content_description_tmdb_logo)
+            )
+            AppSpacer()
+            Text(
+                text = stringResource(id = R.string.data_provided_by_tmdb),
+                fontStyle = FontStyle.Italic
+            )
+            AppSpacer()
+            Text(
+                text = stringResource(id = R.string.tmdb_intro),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
