371c17469f3089453b5b12e1c941576600daee1f
Paul Maltsev <135986470+PaulMaltsev23@users.noreply.github.com>
2024-01-07 11:54:36 +0200

    Create additional screens check app navigation

--- app/src/main/java/com/paulmaltsev/bitmovie/features/home/HomeScreen.kt
+++ app/src/main/java/com/paulmaltsev/bitmovie/features/home/HomeScreen.kt
@@ -1,16 +1,28 @@
 package com.paulmaltsev.bitmovie.features.home
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavController
+import com.paulmaltsev.bitmovie.core.navigation.AppScreens
 
 @Composable
-fun HomeScreen() {
-    Text(
-        "Home screen",
-        modifier = Modifier.fillMaxSize(),
-        textAlign = TextAlign.Center
-    )
+fun HomeScreen(navController: NavController) {
+    Column {
+        Text(
+            "Home screen",
+            textAlign = TextAlign.Center
+        )
+        Button(onClick = { navController.navigate(AppScreens.MovieCategory.route) }) {
+            Text(text = "Go to movie category")
+        }
+        Button(onClick = { navController.navigate(AppScreens.MovieDetails.route) }) {
+            Text(text = "Go to movie details")
+        }
+        Button(onClick = { navController.navigate(AppScreens.TmdbLegalContent.route) }) {
+            Text(text = "Go to tmdb")
+        }
+    }
 }
---
+++ app/src/main/java/com/paulmaltsev/bitmovie/features/legalContent/tmdb/TmdbScreen.kt
@@ -0,0 +1,16 @@
+package com.paulmaltsev.bitmovie.features.legalContent.tmdb
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun TmdbScreen() {
+    Text(
+        "TmdbScreen",
+        modifier = Modifier.fillMaxSize(),
+        textAlign = TextAlign.Center
+    )
+}
